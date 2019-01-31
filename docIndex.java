f9c9dc60d7e80d83d1a3462915751f10a25730c2
Jesse <jreye039@ucr.edu>
2019-01-31 00:31:46 -0800

    added a new class test

--- docIndex.java
+++ docIndex.java
@@ -1,11 +1,14 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.apache.commons.validator.routines.*;
+import org.apache.commons.validator.routines.UrlValidator;
 import javafx.util.Pair;
 
 public class docIndex {
@@ -228,10 +231,38 @@ public class docIndex {
 
 		//create a document index
 		createDocIndex(documents);
-		//System.out.println(docIndex);
 
 		//create a term index
 		createTermIndex(documents);
-		System.out.println(termIndex);
+
+		//test our indexes
+		test tester = new test();
+		tester.runTest(termIndex, docIndex);
     }
+}
+
+class test {
+	public static void runTest(HashMap<String, Pair<Integer, LinkedList<Pair<Integer,Integer>>>> termIndex, HashMap<Integer, Integer> docIndex){
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String query;
+
+		while(true){
+			try{
+				System.out.print("Enter a search term(\"QUIT\" to quit): ");
+				query = in.readLine();
+				if(query.equals("QUIT")){
+					break;
+				}
+				query = query.toLowerCase();
+
+				Pair<Integer, LinkedList<Pair<Integer,Integer>>> postings = termIndex.get(query);
+				System.out.println(postings);
+
+			}catch(Exception e){
+				System.out.println("Your input is invalid!");
+				continue;
+			}
+		}
+	}
 }
